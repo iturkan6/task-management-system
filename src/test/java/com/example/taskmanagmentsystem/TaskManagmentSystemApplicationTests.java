c3d8ac155fa731a42acdd0397736ac59e6cc6ac8
turkan.muradova <turkan.muradova@cybernet.az>
2023-06-21 15:04:15 +0400

    initial commit

---
+++ src/test/java/com/example/taskmanagmentsystem/TaskManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskmanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagmentSystemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
