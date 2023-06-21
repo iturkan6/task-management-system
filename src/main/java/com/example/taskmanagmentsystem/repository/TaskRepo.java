0501aac1ccdd63d616ef5fd5f1380d7a0583b8f0
turkan.muradova <turkan.muradova@cybernet.az>
2023-06-21 16:24:15 +0400

    add repos

---
+++ src/main/java/com/example/taskmanagmentsystem/repository/TaskRepo.java
@@ -0,0 +1,9 @@
+package com.example.taskmanagmentsystem.repository;
+
+import com.example.taskmanagmentsystem.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepo extends JpaRepository<Task, Integer> {
+}
