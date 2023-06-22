69548b25c1e4224541a493f2493cc545aa7d95d9
turkan.muradova <turkan.muradova@cybernet.az>
2023-06-22 17:03:36 +0400

    Add and implement filter endpoint

--- src/main/java/com/example/taskmanagmentsystem/controller/SearchController.java
+++ src/main/java/com/example/taskmanagmentsystem/controller/SearchController.java
@@ -1,2 +1,25 @@
-package com.example.taskmanagmentsystem.controller;public class SearchController {
+package com.example.taskmanagmentsystem.controller;
+
+import com.example.taskmanagmentsystem.dto.TaskRequestDTO;
+import com.example.taskmanagmentsystem.entity.Task;
+import com.example.taskmanagmentsystem.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/search")
+public class SearchController {
+  private final TaskService service;
+
+  @PostMapping
+  public ResponseEntity<List<Task>> search(@RequestBody TaskRequestDTO request) {
+    return ResponseEntity.ok(service.search(request));
+  }
 }
--- src/main/java/com/example/taskmanagmentsystem/service/TaskService.java
+++ src/main/java/com/example/taskmanagmentsystem/service/TaskService.java
@@ -2,11 +2,15 @@ package com.example.taskmanagmentsystem.service;
 
 import com.example.taskmanagmentsystem.configuration.JwtAuthFilter;
 import com.example.taskmanagmentsystem.dto.TaskRequestDTO;
+import com.example.taskmanagmentsystem.dto.TaskStatus;
 import com.example.taskmanagmentsystem.entity.Task;
 import com.example.taskmanagmentsystem.entity.User;
+import com.example.taskmanagmentsystem.exception.TaskNotFoundException;
+import com.example.taskmanagmentsystem.exception.UserNotFoundException;
 import com.example.taskmanagmentsystem.repository.TaskRepo;
 import com.example.taskmanagmentsystem.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,44 +18,53 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class TaskService {
-    private final TaskRepo repo;
-    private final UserRepo userRepo;
-    private final JwtAuthFilter filter;
+  private final TaskRepo repo;
+  private final UserRepo userRepo;
+  private final JwtAuthFilter filter;
 
-    public void createTask(TaskRequestDTO request) {
-        User user = userRepo.findUserByEmail(filter.getEmail()).orElseThrow();
-        Task task = Task.builder()
-                .title(request.title())
-                .description(request.description())
-                .status(request.status())
-                .dueDate(request.dueDate())
-                .user(user)
-                .build();
-        repo.save(task);
-    }
+  public void createTask(TaskRequestDTO request) {
+    User user = userRepo.findUserByEmail(filter.getEmail()).orElseThrow(UserNotFoundException::new);
+    Task task = Task.builder()
+        .title(request.title())
+        .description(request.description())
+        .status(TaskStatus.valueOf(request.status()))
+        .dueDate(request.dueDate())
+        .user(user)
+        .build();
+    repo.save(task);
+  }
 
-    public Task getTaskById(Integer id) {
-        return repo.findById(id).orElseThrow();
-    }
+  public Task getTaskById(Integer id) {
+    return repo.findById(id).orElseThrow(() -> new TaskNotFoundException(id));
+  }
 
-    public List<Task> getAllTasks() {
-        return repo.findAll();
-    }
+  public List<Task> getAllTasks() {
+    return repo.findAll();
+  }
 
-    public Task updateTask(TaskRequestDTO request, Integer id) {
-        Task task = Task.builder()
-                .id(id)
-                .title(request.title())
-                .description(request.description())
-                .status(request.status())
-                .dueDate(request.dueDate())
-                .user(null)
-                .build();
-        repo.save(task);
-        return task;
-    }
+  public Task updateTask(TaskRequestDTO request, Integer id) {
+    Task task = Task.builder()
+        .id(id)
+        .title(request.title())
+        .description(request.description())
+        .status(TaskStatus.valueOf(request.status()))
+        .dueDate(request.dueDate())
+        .user(null)
+        .build();
+    repo.save(task);
+    return task;
+  }
 
-    public void deleteTask(Integer id) {
-            repo.deleteById(id);
-    }
+  public void deleteTask(Integer id) {
+    repo.deleteById(id);
+  }
+
+  public List<Task> search(TaskRequestDTO request) {
+    Task task = Task.builder()
+        .title(request.title())
+        .description(request.description())
+        .status(TaskStatus.valueOf(request.status()))
+        .build();
+    return repo.findAll(Example.of(task));
+  }
 }
