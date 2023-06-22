80a8c8fd75f9477dbe8c8889438c7e343e37566d
iturkan6 <iturkan6@gmail.com>
2023-06-22 08:06:55 +0400

    Improvements in Entities

--- src/main/java/com/example/taskmanagmentsystem/entity/Task.java
+++ src/main/java/com/example/taskmanagmentsystem/entity/Task.java
@@ -1,23 +1,32 @@
 package com.example.taskmanagmentsystem.entity;
 
 import com.example.taskmanagmentsystem.dto.TaskStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
-  @Id
-  private Integer id;
-  @Column(nullable = false)
-  private String title;
-  private String description;
-  private LocalDate dueDate;
-  private TaskStatus status;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private TaskStatus status;
 
-  @ManyToOne
-  @JoinColumn(name = "user_id", nullable = false)
-  private User user;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnore
+    private User user;
 }
--- src/main/java/com/example/taskmanagmentsystem/entity/User.java
+++ src/main/java/com/example/taskmanagmentsystem/entity/User.java
@@ -1,8 +1,10 @@
 package com.example.taskmanagmentsystem.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -12,54 +14,57 @@ import java.util.Set;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Table(name = "_user")
 public class User implements UserDetails {
 
-  @Id
-  Integer id;
-  String name;
-  String surname;
-  @Column(nullable = false, unique = true)
-  String email;
-  @Column(nullable = false, unique = true)
-  String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    String name;
+    String surname;
+    @Column(nullable = false, unique = true)
+    String email;
+    @Column(nullable = false, unique = true)
+    String password;
 
-  @OneToMany(mappedBy = "_user")
-  Set<Task> tasks;
+    @OneToMany
+    Set<Task> tasks;
 
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return List.of();
-  }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
 
-  @Override
-  public String getPassword() {
-    return password;
-  }
+    @Override
+    public String getPassword() {
+        return password;
+    }
 
-  @Override
-  public String getUsername() {
-    return email;
-  }
+    @Override
+    public String getUsername() {
+        return email;
+    }
 
-  @Override
-  public boolean isAccountNonExpired() {
-    return true;
-  }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-  @Override
-  public boolean isAccountNonLocked() {
-    return true;
-  }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return true;
-  }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 
-  @Override
-  public boolean isEnabled() {
-    return true;
-  }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
