e1a522261c5f90a0bda48b8b109e930f64841f5e
turkan.muradova <turkan.muradova@cybernet.az>
2023-06-22 17:00:50 +0400

    add custom exception model

--- src/main/java/com/example/taskmanagmentsystem/dto/ExceptionModel.java
+++ src/main/java/com/example/taskmanagmentsystem/dto/ExceptionModel.java
@@ -1,2 +1,13 @@
-package com.example.taskmanagmentsystem.dto;public class ExceptionModel {
+package com.example.taskmanagmentsystem.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public record ExceptionModel(
+    String error,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
+    LocalDateTime timestamp,
+    int status
+) {
 }
